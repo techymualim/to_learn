7543495eb40a24b8af34b4e2b0321e11bfd696ac
Hassam <syedhassam2001@gmail.com>
2021-05-20 15:41:16 +0500

    check

--- Home_chef.java
+++ Home_chef.java
@@ -5,26 +5,26 @@ public class Home_chef{
      static int price_md;
      static int price_sd;
     
-    Home_chef(String meal_ofd,String sweetdish_ofd,int price_md,int price_sd){
-        this.meal_ofd=meal_ofd;
-        this.sweetdish_ofd=sweetdish_ofd;
-        this.price_md=price_md;
-        this.price_sd=price_sd;
+    Home_chef(String meal_,String sweetdish_,int price_,int price_s){
+        meal_ofd=meal_;
+        sweetdish_ofd=sweetdish_;
+        price_md=price_;
+        price_sd=price_s;
     }
   /* made the methods because asked in the problem that the chef can update the meals,sweets and their prices.*/
-    public void meal_update(String meal_ofd){
-        this.meal_ofd=meal_ofd;
+    public void meal_update(String meal_){
+        meal_ofd=meal_;
     }
-    public void sweet_update(String sweetdish_ofd){
-        this.sweetdish_ofd=sweetdish_ofd;
+    public void sweet_update(String sweetdish_){
+        sweetdish_ofd=sweetdish_;
 
     }
-    public void meal_priceupdate(int price_md){
-        this.price_md=price_md;
+    public void meal_priceupdate(int price_){
+        price_md=price_;
 
     }
-    public void sweet_priceupdate(int price_sd){
-        this.price_sd=price_sd;
+    public void sweet_priceupdate(int price_s){
+        price_sd=price_s;
 
     }
     public void Menu_viewer(){
@@ -37,60 +37,4 @@ public class Home_chef{
     }
 }
 
-public class customer_order extends Home_chef{
-    private String name;
-   private String address;
-   private int phone;
-   private String mode_pay;
-   private String meal;
-   private int total_price;
-   private int Quantity;
-   Scanner myObj = new Scanner(System.in); 
-
-   public void placeOrder(){
-    System.out.println("Enter Name:");
-    this.name=myObj.nextLine();
-    System.out.println("Enter Address:");
-    this.address=myObj.nextLine();
-    System.out.println("Enter phone:");
-    this.phone=myObj.nextInt();
-    System.out.println("Enter Mode Of pay(card or cash on delivery):");
-    this.mode_pay=myObj.nextLine();
-    System.out.println("Select Meal or Sweet");
-    this.meal=myObj.nextLine();
-    System.out.println("Quantity");
-    Quantity=myObj.nextInt();
-    if(meal =="Meal"){
-        this.total_price=price.md*Quantity;
-
-    }else if(meal="Sweet"){
-        this.total_price=price.sd*Quantity;
-    }
-
-   }
-   public void updateOrder(){
-    System.out.println("Enter Name:");
-    this.name=myObj.nextLine();
-    System.out.println("Enter Address:");
-    this.address=myObj.nextLine();
-    System.out.println("Enter phone:");
-    this.phone=myObj.nextInt();
-    System.out.println("Enter Mode Of pay(card or cash on delivery):");
-    this.mode_pay=myObj.nextLine();
-    System.out.println("Select Meal or Sweet");
-    this.meal=myObj.nextLine();
-    System.out.println("Quantity");
-    Quantity=myObj.nextInt();
-    if(meal =="Meal"){
-        this.total_price=price.md*Quantity;
-
-    }else if(meal="Sweet"){
-        this.total_price=price.sd*Quantity;
-    }
-   }
-   public void viewOrder(){
-       System.out.println("Order of "+ this.name+ " Is"+this.meal+"\nTotal Amount is:"+this.total_price);
-
-   }
-   
-}
+ 
